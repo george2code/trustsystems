3b6573b32672ed51e2901e4925e4206756977e57
Yuri Aleksandrov <george2code@gmail.com>
2015-01-05 21:20:09 +0200

    will paginate bootstrap for all reviews and user reviews pages

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'simple_form'
 
 gem 'faker'
 
+gem 'will_paginate-bootstrap'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,10 +1,11 @@
 class ReviewController < ApplicationController
   def index
-    @reviews = Review.all
+    # @reviews = Review.all
+    @reviews = Review.paginate(:page => params[:page], :per_page => 10)
   end
 
   def show
     @user = User.find(params[:id])
-    @reviews = @user.reviews
+    @reviews = @user.reviews.paginate(:page => params[:page], :per_page => 5)
   end
 end
